e374ab2ee66c498e2090b23b73e8bba65f6dec59
Cristopher Garduno Luna <garduno.cris@gmail.com>
2017-03-08 15:14:27 -0800

    Update split2chunk.py

--- split2chunk.py
+++ split2chunk.py
@@ -1,10 +1,16 @@
+################################################################################
+# @author Cristopher Garduno Luna
+# 
+# Extract the 'chunk' of data specific by the chunk param
+################################################################################
+
 def split2chunk(labels, features, chunk):
     # @param  labels   : n x 2 dataframe
     # @param  features : n x m array
     # @param  chunk    : integer - chunk of code to be extracted
     # @return data     : p x m array, where p is the number of samples in the chunk
     #
-    # Extract a specific chunk of data 
+    # Extracts a specific 'chunk' of data 
 
     data = np.hstack((labels, features))  # group labels and features into one matrix
     data = data[labels['chunks']==chunk]  # extract specific 'chunk' of data
