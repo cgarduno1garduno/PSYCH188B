dc9f0d3411a2eb7b5959e1729d75dd5705a47603
Cristopher Garduno Luna <garduno.cris@gmail.com>
2017-03-08 15:16:44 -0800

    Update get_rest.py

--- get_rest.py
+++ get_rest.py
@@ -8,14 +8,14 @@
 def get_rest(labels, features):
     # @param labels   : n x 2 dataframe
     # @param features : n x m array
-    # @return data    : m x 1 array
+    # @return rest    : m x 1 array
     # 
     # Takes in labels & features, then returns numpy array with average value per
     # column of rest data. 
     # To subtract this rest data from the remaining data, use the transpose of 'data'
     
-    data = np.hstack((labels, features))  # put labels and features into one matrix
-    data = data[labels['labels']=='rest'] # get rest data
-    data = np.mean(data[:, 2:], axis=0)   # get average feature values and remove rest label
+    data     = np.hstack((labels, features))  # put labels and features into one matrix
+    all_rest = data[labels['labels']=='rest'] # get rest data
+    rest     = np.mean(data[:, 2:], axis=0)   # get average feature values and remove rest label
     
-    return data
+    return rest
