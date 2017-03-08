090928fa52946f47b7ec8861670280dac726c81b
Cristopher Garduno Luna <garduno.cris@gmail.com>
2017-03-08 13:35:42 -0800

    Update psych188B-cris.py

--- psych188B-cris.py
+++ psych188B-cris.py
@@ -3,34 +3,6 @@
 import os
 import numpy as np
 
-# data.pymvpa.org/datasets/haxby2001
-def load_haxby_data(datapath, sub, mask=None):
-    # input arguments:
-    # datapath (string): path to the root directory
-    # sub (string): subject ID (e.g. subj1, subj2, etc)
-    # output:
-    # maskeddata (numpy array): samples x voxels data matrix
-    # fmrilabel (pandas dataframe): length samples
-    import nibabel as nib
-    import pandas as pd
-    import numpy as np
-
-    fmriobj = nib.load(os.path.join(datapath, sub, 'train.nii.gz'))
-    fmridata, fmriheader = fmriobj.get_data(), fmriobj.header
-    fmridata = np.rollaxis(fmridata, -1)
-    # shift last axis to the first
-    fmrilabel = pd.read_table(os.path.join(datapath, sub, 'labels.txt'), delim_whitespace=True)
-    if mask is not None:
-        maskobj = nib.load(os.path.join(datapath, sub, mask + '.nii.gz'))
-        maskdata, maskheader = maskobj.get_data(), maskobj.header
-        maskeddata = fmridata[:, maskdata > 0]  # timepoints axis 0, voxels axis 1
-        # need to figure out how to mask features back to original geometry
-        print maskeddata.shape
-    else:
-        maskeddata = fmridata
-
-    return maskeddata, fmrilabel[fmrilabel.chunks != 11]  # not loading the testing run that we've set aside
-
 ### Setup working directory & load files
 # the code below assumes that the folder 'haxby2001-188B' is located in your current directory
 cwd = os.getcwd()+'/haxby2001-188B'
